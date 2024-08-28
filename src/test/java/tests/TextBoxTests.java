ac41df5710f046b2924de0f8957c2ac0ea104ceb
welsmond <mrsholder1997@gmail.com>
2024-08-28 21:15:55 +0300

    added lection 3 tasks

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,53 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+
+
+    @Test
+    void fillFormTests() {
+        open("/text-box");
+        $("#userName").setValue("Vladislav");
+        $("#userEmail").setValue("alex@egorov.com");
+        $("#currentAddress").setValue("Some street 1");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+        $("#output #name").shouldHave(text("Vladislav"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+    }
+
+    @Test
+    void fillPracticeFormTests(){
+        open("/automation-practice-form");
+        $("#firstName").setValue("Vladislav");
+        $("#lastName").setValue("Senko");
+        $("#userEmail").setValue("senjkove@gmail.com");
+        $("#permanentAddress").setValue("Another street 1");
+        $("#submit").click();
+        $("#output #name").shouldHave(text("Vladislav"));
+        $("#output #email").shouldHave(text("alex@egorov.com"));
+        $("#output #currentAddress").shouldHave(text("Some street 1"));
+        $("#output #permanentAddress").shouldHave(text("Another street 1"));
+
+    }
+}
